caf6ce5c8e3e4e114fd8ee0658df091753358554
Tso297 <155667055+Tso297@users.noreply.github.com>
2024-01-19 14:27:32 -0500

    Add files via upload

---
+++ ModuleAssignment.py
@@ -0,0 +1,13 @@
+def area():
+    length = int(input('What is the length of the house?' ))
+    width = int(input('What is the width of the house?' ))
+    area = length * width
+    print(area)
+area()
+
+def circumfrence():
+    radius = int(input('What is the radius of the circle? '))
+    circumfrence = 2 * 3.14 * radius
+    print(circumfrence)
+    
+circumfrence()
